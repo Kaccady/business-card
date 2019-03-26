b761f2402fb4e522bd524ee8c83f7d0988b885a1
Kaccady <z-rz@mail.com>
2019-03-26 22:27:14 +0300

    30% of the site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Land from "./land.svg";
+import logo from "./logo.png";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <div className='welcome'>
+        <div className="header">
+          <a href="/">
+            <img className="logo" alt="logo" src={logo} />
           </a>
-        </header>
+        </div>
+        <p className='h1'>I'm Kass.</p>
+        <p className="text1">a web designer.</p>
+        <img className="land" alt="land" src={Land} />
+        </div>
+        <div className='skills'>S
+        <img className="me" alt="Kass" src={logo} />
+        <p className='h2'>Hi.</p>
+        <p className="text2">
+          I'm a web designer / developer based in Moscow, Russia. I have a
+          passion for web design and love to create for web and mobile devices.
+        </p>
+        <p className='h2'>What I can do.</p>
+        </div>S
       </div>
     );
   }
